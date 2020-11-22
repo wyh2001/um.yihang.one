b3b06eb6fe0dbd87bafc8f4940ba04e9a4d0181d
Thunder <31209506+wyh2001@users.noreply.github.com>
2020-11-22 21:27:09 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.22f7e532fa969589bbd3128840cd38d6.js"
+  "precache-manifest.e498f880d9949ac19f2793493054d05b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
